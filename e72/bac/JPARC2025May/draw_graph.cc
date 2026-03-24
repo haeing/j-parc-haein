76418c1e9bbde4903b4144714ab2e9c06904fd87
haeing <hain0817@korea.ac.kr>
2026-03-24 13:54:55 +0900

    T110

--- e72/bac/JPARC2025May/draw_graph.cc
+++ e72/bac/JPARC2025May/draw_graph.cc
@@ -11,12 +11,18 @@ void draw_graph(){
   TFile *f_simul = TFile::Open("../../../data/JPARC2025May/g4_root/T110_simul.root");
   TTree* t_simul = (TTree*)f_simul->Get("tree");
   int nhMppc;
+  double evtposx,evtposy;
   t_simul->SetBranchAddress("nhMppc",&nhMppc);
+  t_simul->SetBranchAddress("evtposx",&evtposx);
+  t_simul->SetBranchAddress("evtposy",&evtposy);
+  
 
   TH1D *h_npe[12];
+  TH2D *h_xy[12];
   TF1 *f_npe[12];
   for(int i=0;i<12;i++){
     h_npe[i] = new TH1D(Form("h_npe%d",i),Form("h_npe%d",i),100,0,100);
+    h_xy[i] = new TH2D(Form("h_xy%d",i),Form("h_xy%d",i),200,-200,200,200,-200,200);
     f_npe[i] = new TF1(Form("f_npe%d",i),"gaus",0,100);
   }
 			    
@@ -24,10 +30,13 @@ void draw_graph(){
     t_beam->GetEntry(n);
     t_simul->GetEntry(n);
     h_npe[seg_bh2]->Fill(nhMppc);
+    h_xy[seg_bh2]->Fill(evtposx,evtposy);
   }
 
   TCanvas *c0 = new TCanvas("c0","c0");
   c0->Divide(4,3);
+  TCanvas *c10 = new TCanvas("c10","c10");
+  c10->Divide(4,3);
   TGraphErrors *g_simul_npe = new TGraphErrors();
   double* x_npe  = g_bac_npe_mean->GetX();
   double* ex_npe = g_bac_npe_mean->GetEX();
@@ -36,7 +45,7 @@ void draw_graph(){
   int pid = 0;
   for(int i=0;i<12;i++){
     c0->cd(i+1);
-    h_npe[i]->Fit(f_npe[i],"RQ");
+    h_npe[i]->Fit(f_npe[i],"RQ","",30,50);
     if(i>2 && i < 10){
       m_npe[pid] = f_npe[i]->GetParameter(1);
       em_npe[pid] = f_npe[i]->GetParError(1);
@@ -45,19 +54,37 @@ void draw_graph(){
       g_simul_npe->SetPointError(pid,ex_npe[pid],em_npe[pid]);
       pid++;
     }
+    c10->cd(i+1);
+    h_xy[i]->Draw("colz");
   }
   
   
   TCanvas *c1 = new TCanvas("c1","c1");
   
-  g_simul_npe->SetMarkerStyle(26);
-  g_simul_npe->SetMarkerSize(2);
+  g_simul_npe->SetMarkerStyle(21);
+  g_bac_npe_mean->SetMarkerColor(kRed);
+  //g_simul_npe->SetMarkerSize(2);
+  TLegend* leg = new TLegend(0.45, 0.55, 0.88, 0.82);
+  leg->SetBorderSize(1);
+  leg->SetFillStyle(0);
+  
+  leg->AddEntry(g_bac_npe_mean,  "Experiment", "p");
+  leg->AddEntry(g_simul_npe,  "Simulation", "p");
+ 
   auto mg = new TMultiGraph();
   mg->Add(g_bac_npe_mean);
   mg->Add(g_simul_npe);
+  mg->GetYaxis()->SetRangeUser(10,50);
+  mg->GetXaxis()->SetTitle("X [mm]");
+  mg->GetYaxis()->SetTitle("Np.e.");
+			   
   mg->Draw("AP");
-
+  leg->Draw("same");
+  
   TCanvas *c2 = new TCanvas("c2","c2");
+  g_eff->GetXaxis()->SetTitle("X [mm]");
+  g_eff->GetYaxis()->SetTitle("Pion Efficiency");
   g_eff->Draw("AP");
+  
   
 }
