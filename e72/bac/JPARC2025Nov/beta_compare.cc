8836181e4f49010ebbd61d60f5b40bb8a3853c52
haeing <hain0817@korea.ac.kr>
2026-05-04 14:45:26 +0900

    bac

--- e72/bac/JPARC2025Nov/beta_compare.cc
+++ e72/bac/JPARC2025Nov/beta_compare.cc
@@ -44,6 +44,25 @@ void beta_compare(){
 
   std::vector<double> x, y, ex, ey;
 
+  TCanvas *c1 = new TCanvas("c1","c1");
+  TString out_pdf = "beta_compare.pdf";
+  TLatex text;
+  text.SetNDC();
+  text.SetTextSize(0.04);
+  TDatime now;
+  TString datetime = Form("%04d-%02d-%02d  %02d:%02d:%02d",
+                        now.GetYear(),
+                        now.GetMonth(),
+                        now.GetDay(),
+                        now.GetHour(),
+                        now.GetMinute(),
+			now.GetSecond());
+  text.DrawLatex(0.3,0.5,Form("Date : %s", datetime.Data()));
+
+  c1->Print(out_pdf+"(");
+  
+  
+  
   for (const auto& r : runs) {
     TString fname = Form("e72_hist_%d.root", r.run);
     TFile* fin = TFile::Open(fname, "READ");
@@ -68,8 +87,8 @@ void beta_compare(){
     double peakX = hfit->GetBinCenter(maxBin);
     double rms   = hfit->GetRMS();
 
-    double fitMin = peakX - 1.0 * rms;
-    double fitMax = peakX + 1.0 * rms;
+    double fitMin = peakX - 10.0 * rms;
+    double fitMax = peakX + 10.0 * rms;
 
     if (rms <= 0) {
       fitMin = peakX - 5.0;
@@ -79,10 +98,15 @@ void beta_compare(){
     if (fitMin < hfit->GetXaxis()->GetXmin()) fitMin = hfit->GetXaxis()->GetXmin();
     if (fitMax > hfit->GetXaxis()->GetXmax()) fitMax = hfit->GetXaxis()->GetXmax();
 
-    TF1* fgaus = new TF1(Form("fgaus_%d", r.run), "gaus", fitMin, fitMax);
+    TF1 * fgaus;
+    if(r.run == 2489)fgaus = new TF1(Form("fgaus_%d", r.run), "gaus", 200, 800);
+    else{fgaus = new TF1(Form("fgaus_%d", r.run), "gaus", fitMin, fitMax);}
     fgaus->SetParameters(hfit->GetMaximum(), peakX, (rms > 0 ? rms/2.0 : 2.0));
 
     hfit->Fit(fgaus, "RQ0");
+    c1->Clear();
+    hfit->Draw();
+    c1->Print(out_pdf);
 
     double mean1  = fgaus->GetParameter(1);
     double sigma1 = std::fabs(fgaus->GetParameter(2));
@@ -90,10 +114,12 @@ void beta_compare(){
     double refitMin = mean1 - 1.5 * sigma1;
     double refitMax = mean1 + 1.5 * sigma1;
 
+    /*
     if (sigma1 > 0 && refitMin < refitMax) {
       fgaus->SetRange(refitMin, refitMax);
       hfit->Fit(fgaus, "RQ0");
     }
+    */
 
     double mean    = fgaus->GetParameter(1);
     double meanErr = fgaus->GetParError(1);
@@ -131,22 +157,29 @@ void beta_compare(){
   gr->SetMarkerSize(1.2);
   gr->SetLineWidth(2);
 
-  TCanvas* c1 = new TCanvas("c1", "npe vs 1/beta^2", 800, 600);
-  gr->Draw("AP");
+  
+  //TCanvas* c1 = new TCanvas("c1", "npe vs 1/beta^2", 800, 600);
+  //gr->Draw("AP");
 
   // 원하면 선형 fit도 가능
   TF1* flin = new TF1("flin", "[0] + [1]*x",
                       *std::min_element(x.begin(), x.end()) - 0.02,
                       *std::max_element(x.begin(), x.end()) + 0.02);
   gr->Fit(flin, "R");
+  //gr->Draw("AP");
+  
+  //c1->SaveAs("npe_mean_vs_invbeta2.pdf");
+  //c1->SaveAs("npe_mean_vs_invbeta2.png");
+
+  c1->Clear();
   gr->Draw("AP");
+  c1->Print(out_pdf+")");
 
-  c1->SaveAs("npe_mean_vs_invbeta2.pdf");
-  c1->SaveAs("npe_mean_vs_invbeta2.png");
-
+  /*
   TFile* fout = new TFile("npe_mean_vs_invbeta2.root", "RECREATE");
   gr->Write();
   if (flin) flin->Write("fit_linear");
   c1->Write();
   fout->Close();
+  */
 }
